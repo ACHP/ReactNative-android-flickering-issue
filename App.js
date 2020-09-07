c8054a222c8e950117991b8ea359c5f0f08d3f99
alexischappron <bullrot-10@hotmail.fr>
2020-09-07 17:05:02 +0200

    Add stylesheet for readability

--- App.js
+++ App.js
@@ -1,73 +1,103 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {
-  Text,
-  StatusBar,
-  ScrollView,
-  View,
-  Image,
+    Text,
+    StatusBar,
+    ScrollView,
+    View,
+    Image,
+    StyleSheet,
 } from 'react-native';
 
 import {text} from './lorem';
 
-const HostNavigationMock = () => (
-    <View style={{flex: 1}}>
-      <View style={{position: 'absolute',top: 0, left: 0, height: '100%', width: '100%', backgroundColor: 'blue'}}>
-        <HomeScreen />
-      </View>
-      <View style={{position: 'absolute',top: 0, left: 0, height: '100%', width: '100%', backgroundColor: 'blue'}}>
-        <BuggyScreen />
-      </View>
-    </View>
-)
-
 const HomeScreen = () => {
-  return (
-    <>
-      <StatusBar
-        backgroundColor="transparent"
-        barStyle="dark-content"
-        translucent
-      />
-        <View style={{flex: 1}}>
-          <View style={{overflow: 'hidden', borderRadius: 2}}>
-            <Image style={{width: 300, aspectRatio: 1.7}} source={{uri: 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg'}} />
-          </View>
-      </View>
-    </>
-  );
+    return (
+        <>
+            <StatusBar
+                backgroundColor="transparent"
+                barStyle="dark-content"
+                translucent={false}
+            />
+            <View style={{flex: 1}}>
+                <View style={homeScreenStylesheet.imageWrapper}>
+                    <Image style={homeScreenStylesheet.image}
+                           source={{uri: 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg'}}/>
+                </View>
+            </View>
+        </>
+    );
 };
+
+const homeScreenStylesheet = StyleSheet.create({
+    imageWrapper: {
+        overflow: 'hidden',
+        borderRadius: 1,
+    },
+    image: {
+        width: 300,
+        height: 300,
+    },
+});
 
 const BuggyScreen = (_props) => {
-  return (
-      <View style={{
-        flex: 1,
-        backgroundColor: 'white',
-      }}
-      >
-        <View
-            style={{
-              position: 'relative',
-              shadowOpacity: 0.2,
-              height: 93.55636467673563,
-              width: 500,
-              backgroundColor: 'white',
-              zIndex: 2,
-            }}
-        />
-        <ScrollView style={{
-          flex: 1,
-          backgroundColor: 'red',
-        }}
-        >
-          <Text>{text}</Text>
-        </ScrollView>
-      </View>
-  );
+    return (
+        <View style={buggedScreenStylesheet.wrapper}>
+            <View style={buggedScreenStylesheet.header}>
+                <Text>Without translucent status bar</Text>
+            </View>
+            <ScrollView style={buggedScreenStylesheet.content}>
+                <Text>{text}</Text>
+            </ScrollView>
+        </View>
+    );
 };
 
+const buggedScreenStylesheet = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+    header: {
+        position: 'relative',
+        shadowOpacity: 0.2,
+        height: 100,
+        width: 500,
+        backgroundColor: 'white',
+        zIndex: 2,
+        justifyContent: 'center',
+    },
+    content: {
+        flex: 1,
+        backgroundColor: 'red',
+    },
+});
+
 export default class App extends React.Component {
-  render() {
-    return <HostNavigationMock />;
-  }
+    render() {
+        return (
+            <View style={appStylesheet.app}>
+                <View style={appStylesheet.screen}>
+                    <HomeScreen/>
+                </View>
+                <View style={appStylesheet.screen}>
+                    <BuggyScreen/>
+                </View>
+            </View>
+        );
+    }
 }
+
+const appStylesheet = StyleSheet.create({
+    app: {
+        flex: 1
+    },
+    screen: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        height: '100%',
+        width: '100%',
+        backgroundColor: 'blue',
+    },
+});
